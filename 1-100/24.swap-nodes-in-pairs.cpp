2b9a8ceddd91db82453710132ef7adaa40255373
Hantao-Ye <hantaoye2000@gmail.com>
2022-08-27 16:15:54 -0700

    Finish Q24

---
+++ 1-100/24.swap-nodes-in-pairs.cpp
@@ -0,0 +1,389 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+int read_int(fstream &in_stream)
+{
+    string temp;
+    getline(in_stream, temp);
+
+    return stoi(temp);
+}
+
+void print_int(int num, fstream &out_stream)
+{
+    out_stream << num;
+}
+
+void print_bool(bool out, fstream &out_stream)
+{
+    if (out)
+        out_stream << "true";
+    else
+        out_stream << "false";
+}
+
+vector<int> read_vector_int(fstream &in_stream)
+{
+    vector<int> nums;
+
+    string temp;
+    getline(in_stream, temp);
+
+    int temp_num = 0, sign = 1;
+    bool empty = true;
+    for (char c : temp)
+    {
+        if (c == '[')
+        {
+            continue;
+        }
+        else if (c >= '0' && c <= '9')
+        {
+            temp_num = temp_num * 10 + c - '0';
+            empty = false;
+        }
+        else if (c == '-')
+        {
+            sign = -1;
+        }
+        else if (c == ',' || (c == ']' && !empty))
+        {
+            nums.push_back(temp_num * sign);
+            temp_num = 0;
+            sign = 1;
+        } else if (c == ']' && empty)
+        {
+            break;
+        }
+    }
+
+    return nums;
+}
+
+vector<string> read_vector_string(fstream &in_stream)
+{
+    vector<string> strs;
+
+    string buffer;
+    getline(in_stream, buffer);
+
+    string temp;
+    for (char c : buffer)
+    {
+        if (c == '[')
+        {
+            continue;
+        }
+        else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        {
+            temp += c;
+        }
+        else if (c == ',' || c == ']')
+        {
+            strs.push_back(temp);
+            temp = "";
+        }
+    }
+
+    return strs;
+}
+
+void print_vector_string(vector<string> strs, fstream &out_stream)
+{
+    if (strs.empty())
+        out_stream << "[]";
+
+    for (int i = 0; i < strs.size(); i++)
+    {
+        if (i == 0)
+        {
+            out_stream << '[';
+        }
+
+        out_stream << strs[i];
+
+        if (i != strs.size() - 1)
+        {
+            out_stream << ',';
+        }
+        else
+        {
+            out_stream << ']';
+        }
+    }
+}
+
+void print_vector_int(vector<int> nums, fstream &out_stream)
+{
+    if (nums.empty())
+        out_stream << "[]";
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (i == 0)
+        {
+            out_stream << '[';
+        }
+
+        out_stream << nums[i];
+
+        if (i != nums.size() - 1)
+        {
+            out_stream << ',';
+        }
+        else
+        {
+            out_stream << ']';
+        }
+    }
+}
+
+void print_vector_vector_int(vector<vector<int>> nums, fstream &out_stream)
+{
+    if (nums.empty())
+        out_stream << "[]";
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (i == 0)
+        {
+            out_stream << '[';
+        }
+
+        print_vector_int(nums[i], out_stream);
+
+        if (i != nums.size() - 1)
+        {
+            out_stream << ',';
+        }
+        else
+        {
+            out_stream << ']';
+        }
+    }
+}
+
+ListNode *read_list_node(fstream &in_stream)
+{
+    string temp;
+    getline(in_stream, temp);
+
+    ListNode *head = new ListNode();
+    ListNode *ptr = head;
+
+    int temp_num = 0, sign = 1;
+    bool empty = true;
+
+    for (char c : temp)
+    {
+        if (c == '[')
+        {
+            continue;
+        }
+        else if (c >= '0' && c <= '9')
+        {
+            temp_num = temp_num * 10 + c - '0';
+            empty = false;
+        }
+        else if (c == '-')
+        {
+            sign = -1;
+        }
+        else if (c == ',' || (c == ']' && !empty))
+        {
+            ptr->next = new ListNode(sign * temp_num);
+
+            ptr = ptr->next;
+
+            temp_num = 0;
+            sign = 1;
+        } else if (c == ']' && empty) {
+            break;
+        }
+    }
+
+    return head->next;
+}
+
+vector<ListNode *> read_vector_list_node(fstream &in_stream)
+{
+    string temp;
+    getline(in_stream, temp);
+
+    vector<ListNode *> lists;
+    ListNode *head, *ptr;
+
+    int temp_num = 0, sign = 1;
+    bool insert = false;
+    bool empty = true;
+
+    for (char c : temp)
+    {
+        if (c == '[')
+        {
+            head = new ListNode();
+            ptr = head;
+
+            insert = true;
+            empty = true;
+        }
+        else if (c >= '0' && c <= '9')
+        {
+            temp_num = temp_num * 10 + c - '0';
+            empty = false;
+        }
+        else if (c == '-')
+        {
+            sign = -1;
+        }
+        else if (c == ',' && insert)
+        {
+            ptr->next = new ListNode(sign * temp_num);
+
+            ptr = ptr->next;
+
+            temp_num = 0;
+            sign = 1;
+        }
+        else if (c == ']' && empty)
+        {
+            lists.push_back(head->next);
+
+            temp_num = 0;
+            sign = 1;
+
+            insert = false;
+        }
+        else if (c == ']' && insert)
+        {
+            ptr->next = new ListNode(sign * temp_num);
+            lists.push_back(head->next);
+
+            temp_num = 0;
+            sign = 1;
+
+            insert = false;
+        }
+    }
+
+    return lists;
+}
+
+void print_list_node(ListNode *head, fstream &out_stream)
+{
+    out_stream << '[';
+    while (head)
+    {
+        out_stream << head->val;
+        if (head->next)
+            out_stream << ',';
+
+        head = head->next;
+    }
+    out_stream << ']';
+}
+
+string read_string(fstream &in_stream)
+{
+    string temp;
+
+    getline(in_stream, temp);
+
+    return temp;
+}
+
+void print_string(string str, fstream &out_stream)
+{
+    out_stream << str;
+}
+/*
+ * @lc app=leetcode id=24 lang=cpp
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode* dummy_head = new ListNode(0, head);
+        
+        ListNode* prev = dummy_head;
+        ListNode* first = prev->next;
+
+        while (first && first->next) {
+            ListNode* second = first->next;
+            prev->next = second;
+            first->next = second->next;
+            second->next = first;
+
+            prev = first;
+            first = prev->next;
+        }
+
+        return dummy_head->next;
+    }
+};
+// @lc code=end
+
+int main()
+{
+    Solution sol = Solution();
+
+    fstream in_txt("in.txt");
+    fstream out_txt("out.txt", ios::out | ios::trunc);
+
+    if (in_txt.is_open() && out_txt.is_open())
+    {
+        int n = read_int(in_txt);
+        for (int i = 0; i < n; i++)
+        {
+            ListNode* head = read_list_node(in_txt);
+            print_list_node(sol.swapPairs(head), out_txt);
+
+            if (i != n - 1)
+            {
+                out_txt << endl;
+            }
+        }
+
+        in_txt.close();
+        out_txt.close();
+    }
+    else
+    {
+        cout << "File does not open!" << endl;
+    }
+
+    return 0;
+}
