de63936b54e12ad84f98533f9d2dbc0bd56b3ead
Hantao-Ye <hantaoye2000@gmail.com>
2022-09-08 16:47:22 -0700

    Review Q39

--- 1-100/39.combination-sum.cpp
+++ 1-100/39.combination-sum.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <unordered_map>
 #include <unordered_set>
 #include <string>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -45,30 +47,182 @@ vector<int> read_vector_int(fstream &in_stream)
     getline(in_stream, temp);
 
     int temp_num = 0, sign = 1;
+    bool empty = true;
     for (char c : temp)
     {
-        if (c >= '0' && c <= '9')
+        if (c == '[')
+        {
+            continue;
+        }
+        else if (c >= '0' && c <= '9')
         {
             temp_num = temp_num * 10 + c - '0';
+            empty = false;
         }
         else if (c == '-')
         {
             sign = -1;
         }
-        else
+        else if (c == ',' || (c == ']' && !empty))
         {
             nums.push_back(temp_num * sign);
             temp_num = 0;
             sign = 1;
         }
+        else if (c == ']' && empty)
+        {
+            break;
+        }
     }
-    nums.push_back(temp_num);
 
     return nums;
 }
 
-void print_vector(vector<int> nums, fstream &out_stream)
+vector<string> read_vector_string(fstream &in_stream)
 {
+    vector<string> strs;
+
+    string buffer;
+    getline(in_stream, buffer);
+
+    string temp;
+    for (char c : buffer)
+    {
+        if (c == '[')
+        {
+            continue;
+        }
+        else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        {
+            temp += c;
+        }
+        else if (c == ',' || c == ']')
+        {
+            strs.push_back(temp);
+            temp = "";
+        }
+    }
+
+    return strs;
+}
+
+vector<vector<char>> read_vector_vector_char(fstream &in_stream)
+{
+    vector<vector<char>> board;
+
+    bool insert = false;
+    bool nextLine = true;
+    while (nextLine)
+    {
+        string buffer;
+        getline(in_stream, buffer);
+
+        vector<char> line;
+        for (char c : buffer)
+        {
+            if (c == '[')
+            {
+                insert = true;
+            }
+            else if (c != ',' && c != ']')
+            {
+                line.push_back(c);
+            }
+            else if (c == ',')
+            {
+                continue;
+            }
+            else if (c == ']' && insert)
+            {
+                insert = false;
+                board.push_back(line);
+                line.clear();
+            }
+            else if (c == ']' && !insert)
+            {
+                nextLine = false;
+            }
+        }
+    }
+
+    return board;
+}
+
+void print_vector_char(vector<char> strs, fstream &out_stream)
+{
+    if (strs.empty())
+        out_stream << "[]";
+
+    for (int i = 0; i < strs.size(); i++)
+    {
+        if (i == 0)
+        {
+            out_stream << '[';
+        }
+
+        out_stream << strs[i];
+
+        if (i != strs.size() - 1)
+        {
+            out_stream << ',';
+        }
+        else
+        {
+            out_stream << ']';
+        }
+    }
+}
+
+void print_vector_vector_char(vector<vector<char>> board, fstream &out_stream)
+{
+    for (int i = 0; i < board.size(); i++)
+    {
+        if (i == 0)
+        {
+            out_stream << '[';
+        } else {
+            out_stream << ',';
+        }
+
+        print_vector_char(board[i], out_stream);
+        if (i == board.size() - 1)
+        {
+            out_stream << ']';
+        }
+        out_stream << endl;
+    }
+}
+
+void print_vector_string(vector<string> strs, fstream &out_stream)
+{
+    if (strs.empty())
+        out_stream << "[]";
+
+    for (int i = 0; i < strs.size(); i++)
+    {
+        if (i == 0)
+        {
+            out_stream << '[';
+        }
+
+        out_stream << strs[i];
+
+        if (i != strs.size() - 1)
+        {
+            out_stream << ',';
+        }
+        else
+        {
+            out_stream << ']';
+        }
+    }
+}
+
+void print_vector_int(vector<int> nums, fstream &out_stream)
+{
+    if (nums.empty())
+        out_stream << "[]";
+
     for (int i = 0; i < nums.size(); i++)
     {
         if (i == 0)
@@ -89,15 +243,47 @@ void print_vector(vector<int> nums, fstream &out_stream)
     }
 }
 
-void print_vector_vector(vector<vector<int>> nums, fstream &out_stream)
+void print_vector_int_k(vector<int> nums, int k, fstream &out_stream)
 {
-    for(int i = 0; i < nums.size(); i++)
+    if (nums.empty())
+        out_stream << "[]";
+
+    if (k > nums.size())
+        k = nums.size();
+
+    for (int i = 0; i < k; i++)
     {
-        if (i == 0) {
+        if (i == 0)
+        {
             out_stream << '[';
         }
 
-        print_vector(nums[i], out_stream);
+        out_stream << nums[i];
+
+        if (i != k - 1)
+        {
+            out_stream << ',';
+        }
+        else
+        {
+            out_stream << ']';
+        }
+    }
+}
+
+void print_vector_vector_int(vector<vector<int>> nums, fstream &out_stream)
+{
+    if (nums.empty())
+        out_stream << "[]";
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (i == 0)
+        {
+            out_stream << '[';
+        }
+
+        print_vector_int(nums[i], out_stream);
 
         if (i != nums.size() - 1)
         {
@@ -119,17 +305,24 @@ ListNode *read_list_node(fstream &in_stream)
     ListNode *ptr = head;
 
     int temp_num = 0, sign = 1;
+    bool empty = true;
+
     for (char c : temp)
     {
-        if (c >= '0' && c <= '9')
+        if (c == '[')
+        {
+            continue;
+        }
+        else if (c >= '0' && c <= '9')
         {
             temp_num = temp_num * 10 + c - '0';
+            empty = false;
         }
         else if (c == '-')
         {
             sign = -1;
         }
-        else
+        else if (c == ',' || (c == ']' && !empty))
         {
             ptr->next = new ListNode(sign * temp_num);
 
@@ -138,21 +331,91 @@ ListNode *read_list_node(fstream &in_stream)
             temp_num = 0;
             sign = 1;
         }
+        else if (c == ']' && empty)
+        {
+            break;
+        }
     }
-    ptr->next = new ListNode(temp_num);
 
     return head->next;
+}
+
+vector<ListNode *> read_vector_list_node(fstream &in_stream)
+{
+    string temp;
+    getline(in_stream, temp);
+
+    vector<ListNode *> lists;
+    ListNode *head, *ptr;
+
+    int temp_num = 0, sign = 1;
+    bool insert = false;
+    bool empty = true;
+
+    for (char c : temp)
+    {
+        if (c == '[')
+        {
+            head = new ListNode();
+            ptr = head;
+
+            insert = true;
+            empty = true;
+        }
+        else if (c >= '0' && c <= '9')
+        {
+            temp_num = temp_num * 10 + c - '0';
+            empty = false;
+        }
+        else if (c == '-')
+        {
+            sign = -1;
+        }
+        else if (c == ',' && insert)
+        {
+            ptr->next = new ListNode(sign * temp_num);
+
+            ptr = ptr->next;
+
+            temp_num = 0;
+            sign = 1;
+        }
+        else if (c == ']' && empty)
+        {
+            lists.push_back(head->next);
+
+            temp_num = 0;
+            sign = 1;
+
+            insert = false;
+        }
+        else if (c == ']' && insert)
+        {
+            ptr->next = new ListNode(sign * temp_num);
+            lists.push_back(head->next);
+
+            temp_num = 0;
+            sign = 1;
+
+            insert = false;
+        }
+    }
+
+    return lists;
 }
 
 void print_list_node(ListNode *head, fstream &out_stream)
 {
     out_stream << '[';
-    while (head->next)
+    while (head)
     {
-        out_stream << head->val << ',';
+        out_stream << head->val;
+        if (head->next)
+            out_stream << ',';
+
         head = head->next;
     }
-    out_stream << head->val << ']';
+    out_stream << ']';
 }
 
 string read_string(fstream &in_stream)
@@ -180,32 +443,33 @@ class Solution
 public:
     vector<vector<int>> combinationSum(vector<int> &candidates, int target)
     {
-        ans.clear();
+        ans = vector<vector<int>>();
+        sort(candidates.begin(), candidates.end());
 
-        helper({}, candidates, target, 0, 0);
+        helper(candidates, target, {}, 0, 0);
 
         return ans;
     }
 
 private:
     vector<vector<int>> ans;
-    void helper(vector<int> temp, vector<int> &candidates, int target, int sum, int index)
+
+    void helper(vector<int> &candidates, int target, vector<int> temp, int sum, int start)
     {
-        if (target == sum)
+        if (sum > target)
+        {
+            return;
+        }
+        else if (sum == target)
         {
             ans.push_back(temp);
             return;
         }
-        else if (target < sum)
-        {
-            return;
-        }
 
-        for (int i = index; i < candidates.size(); i++) {
-            sum += candidates[i];
+        for (int i = start; i < candidates.size(); i++)
+        {
             temp.push_back(candidates[i]);
-            helper(temp, candidates, target, sum, i);
-            sum -= candidates[i];
+            helper(candidates, target, temp, sum + candidates[i], i);
             temp.pop_back();
         }
     }
@@ -227,7 +491,7 @@ int main()
             vector<int> candidates = read_vector_int(in_txt);
             int target = read_int(in_txt);
 
-            print_vector_vector(s.combinationSum(candidates, target), out_txt);
+            print_vector_vector_int(s.combinationSum(candidates, target), out_txt);
 
             if (i != n - 1)
             {
